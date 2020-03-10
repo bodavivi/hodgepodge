52e70e4f44a948652397746a4158b5a572b2e670
Boda <bodavivienviktoria@gmail.com>
2020-03-10 13:54:17 +0100

    test(test/ingredients) : Add ChickenLegTest, it runs.

---
+++ src/test/java/com/lavien/hodgepodge/models/ChickenLegTest.java
@@ -0,0 +1,25 @@
+package com.lavien.hodgepodge.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChickenLegTest {
+  ChickenLeg test = new ChickenLeg();
+
+  @Test
+  public void ChickenLegValue_4_True() {
+    assertEquals(test.getValue(), 4);
+  }
+
+  @Test
+  public void ChickenLegValue_5_False() {
+    assertNotEquals(test.getValue(), 5);
+  }
+
+  @Test
+  public void ChickenLegValue_3_False() {
+    assertNotEquals(test.getValue(), 3);
+  }
+
+}
