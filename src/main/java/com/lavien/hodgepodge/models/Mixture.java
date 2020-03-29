45dea38154ae81ed22c002eb57b1198917311225
Boda <bodavivienviktoria@gmail.com>
2020-03-29 17:27:17 +0200

    feat(feature/GameMixtureRelation) : Relations between Game and Mixture are updated.

--- src/main/java/com/lavien/hodgepodge/models/Mixture.java
+++ src/main/java/com/lavien/hodgepodge/models/Mixture.java
@@ -2,33 +2,40 @@ package com.lavien.hodgepodge.models;
 
 import com.lavien.hodgepodge.models.ingredients.Ingredient;
 
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+@Entity
 public class Mixture {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-  @ManyToMany
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+  @JoinTable(name = "unavailable_mixture_game",
+      joinColumns = @JoinColumn(name = "mixture_id"),
+  inverseJoinColumns = @JoinColumn(name = "game_id"))
+  private List<Game> gamesWhereUnavailable;
+
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+  @JoinTable(name = "available_mixture_game",
+      joinColumns = @JoinColumn(name = "mixture_id"),
+      inverseJoinColumns = @JoinColumn(name = "game_id"))
+  private List<Game> gamesWhereAvailable;
+
+  @Transient
   private HashMap<Ingredient, Integer> ingredients;
   private int point;
 
-  public Mixture(HashMap<Ingredient, Integer> ingredients, int point) {
-    this.ingredients = ingredients;
-    this.point = point;
+  public Mixture() {
   }
 
-  public HashMap<Ingredient, Integer> getIngredients() {
-    return ingredients;
-  }
-
-  public void setIngredients(HashMap<Ingredient, Integer> ingredients) {
-    this.ingredients = ingredients;
-  }
-
-  public int getPoint() {
-    return point;
-  }
-
-  public void setPoint(int point) {
+  public Mixture(int point) {
+    this.gamesWhereUnavailable = new ArrayList<>();
+    this.gamesWhereAvailable = new ArrayList<>();
+    this.ingredients = new HashMap<>();
     this.point = point;
   }
 }
