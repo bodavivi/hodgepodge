a98afe84e1feafa52ae4aa6fbb1c94d825f4797c
Boda <bodavivienviktoria@gmail.com>
2020-03-30 17:13:22 +0200

    feat(feature/MixtureIngredient) : Mixtures Ingredient relation is rethought.

--- src/main/java/com/lavien/hodgepodge/models/Mixture.java
+++ src/main/java/com/lavien/hodgepodge/models/Mixture.java
@@ -23,11 +23,10 @@ public class Mixture {
       inverseJoinColumns = @JoinColumn(name = "game_id"))
   private List<Game> gamesWhereAvailable;
 
-  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-  @JoinTable(name = "mixture_ingredient",
-      joinColumns = @JoinColumn(name = "mixture_id"),
-      inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
-  private List<Ingredient> ingredients;
+  private int ingRoot;
+  private int ingMushroom;
+  private int ingFeather;
+  private int ingChickenLeg;
 
   @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
   @JoinTable(name = "mixture_alchemist",
@@ -40,11 +39,14 @@ public class Mixture {
   public Mixture() {
   }
 
-  public Mixture(int point, List<Ingredient> ingredients) {
+  public Mixture(int point, int ingRoot, int ingMushroom, int ingFeather, int ingChickenLeg) {
     this.gamesWhereUnavailable = new ArrayList<>();
     this.gamesWhereAvailable = new ArrayList<>();
-    this.ingredients = ingredients;
     this.alchemists = new ArrayList<>();
     this.point = point;
+    this.ingRoot = ingRoot;
+    this.ingMushroom = ingMushroom;
+    this.ingFeather = ingFeather;
+    this.ingChickenLeg = ingChickenLeg;
   }
 }
