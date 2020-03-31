cbe922c4c7ef2fcb6330b709fafb1c3bfe9eff91
Boda <bodavivienviktoria@gmail.com>
2020-03-31 18:50:48 +0200

    feature(feature/MixtureCoin) : Coin fields in Mixture model are added.

--- src/main/java/com/lavien/hodgepodge/models/Mixture.java
+++ src/main/java/com/lavien/hodgepodge/models/Mixture.java
@@ -36,7 +36,15 @@ public class Mixture {
 
   private int point;
 
+  private int goldCoin;
+  private int silverCoin;
+
   public Mixture() {
+    this.gamesWhereUnavailable = new ArrayList<>();
+    this.gamesWhereAvailable = new ArrayList<>();
+    this.alchemists = new ArrayList<>();
+    this.goldCoin = 0;
+    this.silverCoin = 0;
   }
 
   public Mixture(int point, int ingrRoot, int ingrMushroom, int ingrFeather, int ingrChickenLeg) {
@@ -48,5 +56,95 @@ public class Mixture {
     this.ingrMushroom = ingrMushroom;
     this.ingrFeather = ingrFeather;
     this.ingrChickenLeg = ingrChickenLeg;
+    this.goldCoin = 0;
+    this.silverCoin = 0;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public List<Game> getGamesWhereUnavailable() {
+    return gamesWhereUnavailable;
+  }
+
+  public void setGamesWhereUnavailable(List<Game> gamesWhereUnavailable) {
+    this.gamesWhereUnavailable = gamesWhereUnavailable;
+  }
+
+  public List<Game> getGamesWhereAvailable() {
+    return gamesWhereAvailable;
+  }
+
+  public void setGamesWhereAvailable(List<Game> gamesWhereAvailable) {
+    this.gamesWhereAvailable = gamesWhereAvailable;
+  }
+
+  public int getIngrRoot() {
+    return ingrRoot;
+  }
+
+  public void setIngrRoot(int ingrRoot) {
+    this.ingrRoot = ingrRoot;
+  }
+
+  public int getIngrMushroom() {
+    return ingrMushroom;
+  }
+
+  public void setIngrMushroom(int ingrMushroom) {
+    this.ingrMushroom = ingrMushroom;
+  }
+
+  public int getIngrFeather() {
+    return ingrFeather;
+  }
+
+  public void setIngrFeather(int ingrFeather) {
+    this.ingrFeather = ingrFeather;
+  }
+
+  public int getIngrChickenLeg() {
+    return ingrChickenLeg;
+  }
+
+  public void setIngrChickenLeg(int ingrChickenLeg) {
+    this.ingrChickenLeg = ingrChickenLeg;
+  }
+
+  public List<Alchemist> getAlchemists() {
+    return alchemists;
+  }
+
+  public void setAlchemists(List<Alchemist> alchemists) {
+    this.alchemists = alchemists;
+  }
+
+  public int getPoint() {
+    return point;
+  }
+
+  public void setPoint(int point) {
+    this.point = point;
+  }
+
+  public int getGoldCoin() {
+    return goldCoin;
+  }
+
+  public void setGoldCoin(int goldCoin) {
+    this.goldCoin = goldCoin;
+  }
+
+  public int getSilverCoin() {
+    return silverCoin;
+  }
+
+  public void setSilverCoin(int silverCoin) {
+    this.silverCoin = silverCoin;
   }
 }
