b7d4f4ee7b2ead9df63687a255c44a84e2f620de
Boda <bodavivienviktoria@gmail.com>
2020-04-06 11:40:44 +0200

    feature(feature/GameAlchemistRandomize) : Alchemist Randomizer method to the game setup method is added.

--- src/main/java/com/lavien/hodgepodge/services/GameServiceImpl.java
+++ src/main/java/com/lavien/hodgepodge/services/GameServiceImpl.java
@@ -2,7 +2,11 @@ package com.lavien.hodgepodge.services;
 
 import com.lavien.hodgepodge.models.Alchemist;
 import com.lavien.hodgepodge.models.Game;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +14,20 @@ public class GameServiceImpl implements GameService {
 
   @Override
   public Game setUp(List<Alchemist> alchemists) {
-    return null;
+    Game game = new Game();
+    game.setAlchemists(randomizeAlchemist(alchemists));
+    return game;
+  }
+
+  private ArrayList<Alchemist> randomizeAlchemist(List<Alchemist> alchemists) {
+    ArrayList<Alchemist> alchemistsRandomOrder = new ArrayList<>();
+    Random random = new Random();
+    for (int i = 0; i < alchemists.size(); i++) {
+      int nextAlchemist = random.nextInt(alchemists.size());
+      alchemistsRandomOrder.add(alchemists.get(nextAlchemist));
+      alchemists.remove(nextAlchemist);
+      i--;
+    }
+    return alchemistsRandomOrder;
   }
 }
