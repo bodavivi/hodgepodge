06fd5721d3b1fcb6ed38d04c754791a68f6962eb
Boda <bodavivienviktoria@gmail.com>
2020-04-06 17:30:08 +0200

    feature(feature/MerchantEntityAnnotations) : Spring application is clear

--- src/main/java/com/lavien/hodgepodge/HodgepodgeApplication.java
+++ src/main/java/com/lavien/hodgepodge/HodgepodgeApplication.java
@@ -1,15 +1,10 @@
 package com.lavien.hodgepodge;
 
-import com.lavien.hodgepodge.repositories.MerchantRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HodgepodgeApplication{
-  @Autowired
-  MerchantRepository merchantRepository;
-
+public class HodgepodgeApplication {
   public static void main(String[] args) {
     SpringApplication.run(HodgepodgeApplication.class, args);
   }
