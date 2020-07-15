2153a3ee9964ecf3b6fb5ad3322a39dd5c8e385b
X4DD3W <xaddew@gmail.com>
2020-07-15 11:54:00 +0200

    refactor(MerchantRepository): remove unused import

--- src/main/java/com/lavien/hodgepodge/repositories/MerchantRepository.java
+++ src/main/java/com/lavien/hodgepodge/repositories/MerchantRepository.java
@@ -1,7 +1,6 @@
 package com.lavien.hodgepodge.repositories;
 
 import com.lavien.hodgepodge.models.merchants.Merchant;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
