377c9a4cf7b2d8bec8037a0df3bed1575d8eb5cc
X4DD3W <xaddew@gmail.com>
2020-09-18 11:52:24 +0200

    :art: refactor: edit MerchantServiceImplTest

--- src/test/java/com/lavien/hodgepodge/services/MerchantServiceImplTest.java
+++ src/test/java/com/lavien/hodgepodge/services/MerchantServiceImplTest.java
@@ -1,43 +1,39 @@
 package com.lavien.hodgepodge.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.lavien.hodgepodge.models.merchants.Merchant;
 import com.lavien.hodgepodge.models.merchants.ObtainIngrMerchant;
-import com.lavien.hodgepodge.models.merchants.TradeIngrMerchant;
 import com.lavien.hodgepodge.models.merchants.UpdateIngrMerchant;
 import com.lavien.hodgepodge.repositories.MerchantRepository;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class MerchantServiceImplTest {
 
   private MerchantServiceImpl merchantService;
   private MerchantRepository merchantRepository;
+  private List<Merchant> testMerchants;
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
     this.merchantRepository = mock(MerchantRepository.class);
     this.merchantService = new MerchantServiceImpl(merchantRepository);
+    this.testMerchants = List.of(
+        new ObtainIngrMerchant(2, 0, 0, 0),
+        new UpdateIngrMerchant(2));
   }
 
   @Test
   public void findAll_ReturnsAllPredefinedMerchants_ThereAreTwoMerchants() {
-    Merchant one = new ObtainIngrMerchant(1, 1, 1, 1);
-    Merchant two = new UpdateIngrMerchant(3);
-    List<Merchant> expected = new ArrayList<>(Arrays.asList(one, two));
-
-    when(merchantRepository.findAll()).thenReturn(new ArrayList<>(Arrays.asList(one, two)));
-    assertEquals(expected, merchantService.findAll());
+    when(merchantRepository.findAll()).thenReturn(testMerchants);
+    assertEquals(testMerchants, merchantService.findAll());
     verify(merchantRepository).findAll();
   }
 
@@ -59,32 +55,22 @@ public class MerchantServiceImplTest {
 
   @Test
   public void findStarterUnavailableMerchants_ReturnsAllMerchantExceptStarters_ThereIsADatabaseWithMerchants() {
-    List<Merchant> merchants = List.of(
-        new ObtainIngrMerchant(1, 2, 3, 4),
-        new TradeIngrMerchant(1, 1, 1, 1, 0, 0, 0, 0),
-        new UpdateIngrMerchant(3));
+    this.testMerchants.get(0).setId(11L);
+    this.testMerchants.get(1).setId(12L);
 
-    merchants.get(0).setId(11L);
-    merchants.get(1).setId(12L);
-    merchants.get(2).setId(13L);
-
-    when(merchantRepository.findAll()).thenReturn(merchants);
-    assertEquals(merchants, merchantService.findStarterUnavailableMerchants());
+    when(merchantRepository.findAll()).thenReturn(testMerchants);
+    assertEquals(testMerchants, merchantService.findStarterUnavailableMerchants());
     verify(merchantRepository).findAll();
   }
 
   @Test
   public void pickUpStarterCards_ReturnsBasicMerchants_ThereAreTwoMerchantCards() {
-    List<Merchant> merchants = List.of(
-        new ObtainIngrMerchant(2, 0, 0, 0),
-        new UpdateIngrMerchant(2));
+    this.testMerchants.get(0).setId(1L);
+    this.testMerchants.get(1).setId(2L);
 
-    merchants.get(0).setId(1L);
-    merchants.get(1).setId(2L);
-
-    when(merchantRepository.findById(1L)).thenReturn(Optional.of(merchants.get(0)));
-    when(merchantRepository.findById(2L)).thenReturn(Optional.of(merchants.get(1)));
-    assertEquals(merchants, merchantService.pickUpStarterCards());
+    when(merchantRepository.findById(1L)).thenReturn(Optional.of(testMerchants.get(0)));
+    when(merchantRepository.findById(2L)).thenReturn(Optional.of(testMerchants.get(1)));
+    assertEquals(testMerchants, merchantService.pickUpStarterCards());
     verify(merchantRepository).findById(1L);
     verify(merchantRepository).findById(2L);
   }
