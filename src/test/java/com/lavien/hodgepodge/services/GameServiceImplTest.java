043cd9e647ed848f6c21fc96a9456a3b81b9cf30
X4DD3W <xaddew@gmail.com>
2020-10-07 12:24:06 +0200

    :art: feat: add tests for setting up Merchants

--- src/test/java/com/lavien/hodgepodge/services/GameServiceImplTest.java
+++ src/test/java/com/lavien/hodgepodge/services/GameServiceImplTest.java
@@ -15,6 +15,10 @@ import com.lavien.hodgepodge.exceptions.GameNotFoundException;
 import com.lavien.hodgepodge.models.Alchemist;
 import com.lavien.hodgepodge.models.Game;
 import com.lavien.hodgepodge.models.Mixture;
+import com.lavien.hodgepodge.models.merchants.Merchant;
+import com.lavien.hodgepodge.models.merchants.ObtainIngrMerchant;
+import com.lavien.hodgepodge.models.merchants.TradeIngrMerchant;
+import com.lavien.hodgepodge.models.merchants.UpdateIngrMerchant;
 import com.lavien.hodgepodge.repositories.AlchemistRepository;
 import com.lavien.hodgepodge.repositories.GameRepository;
 import java.util.ArrayList;
@@ -37,158 +41,31 @@ class GameServiceImplTest {
   private GameServiceImpl gameService;
   private GameRepository gameRepository;
   private MixtureService mixtureService;
+  private MerchantService merchantService;
   private List<Alchemist> alchemists;
   public static final String GAMECODE = "testCode";
   public List<Mixture> mixtures;
+  public List<Merchant> merchants;
   public Random randomNumberMock;
 
   @BeforeEach
   public void setUp() {
     this.gameRepository = mock(GameRepository.class);
     this.mixtureService = mock(MixtureService.class);
+    this.merchantService = mock(MerchantService.class);
     this.randomNumberMock = mock(Random.class);
+
     this.gameService = new GameServiceImpl(
-        this.mixtureService, mock(MerchantService.class),
+        this.mixtureService, this.merchantService,
         this.gameRepository, mock(AlchemistRepository.class),
         this.randomNumberMock);
 
     this.alchemists = new ArrayList<>(Arrays.asList(new Alchemist(), new Alchemist()));
     this.mixtures = new ArrayList<>(Arrays.asList(new Mixture(10, 1, 1, 1, 1), new Mixture(11, 2, 2, 2, 2)));
-  }
-
-  @Test
-  public void getAll_ReturnAllGames_TwoGames() {
-    Game test1 = new Game(GAMECODE + "1");
-    Game test2 = new Game(GAMECODE + "2");
-    List<Game> games = new ArrayList<>(Arrays.asList(test1, test2));
-
-    when(this.gameRepository.findAll()).thenReturn(games);
-
-    List<Game> result = this.gameService.getAll();
-
-    assertNotNull(result);
-    assertEquals(2, result.size());
-    assertEquals(games, this.gameService.getAll());
-
-    verify(this.gameRepository, times(2)).findAll();
-  }
-
-  @Test
-  public void getAll_ReturnEmptyList_NoGame() {
-    when(this.gameRepository.findAll()).thenReturn(new ArrayList<>());
-    assertEquals(new ArrayList<>(), this.gameService.getAll());
-    verify(this.gameRepository).findAll();
-  }
-
-  @Test
-  public void getGameByGameCode_ReturnGame_GameWithExistingGameCode() {
-    Game test = new Game(GAMECODE);
-
-    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(test));
-
-    Game result = this.gameService.getGameByGameCode(GAMECODE);
-
-    assertNotNull(result);
-    assertEquals(GAMECODE, result.getGameCode());
-    assertEquals(test, this.gameService.getGameByGameCode(GAMECODE));
-
-    verify(this.gameRepository, times(2)).getGameByGameCode(GAMECODE);
-  }
-
-  @Test
-  public void getGameByGameCode_ThrowGameNotFoundException_GameWithNonExistingGameCode() {
-    when(this.gameRepository.getGameByGameCode("notExist")).thenReturn(Optional.empty());
-    assertThrows(GameNotFoundException.class, () -> this.gameService.getGameByGameCode("notExist"));
-  }
-
-  @Test
-  public void deleteById_SoftDeleteGame_ValidId() {
-    Game test = new Game(GAMECODE);
-    test.setId(1L);
-
-    when(this.gameRepository.findById(1L)).thenReturn(Optional.of(test));
-    doNothing().when(this.gameRepository).deleteById(1L);
-
-    this.gameService.deleteById(1L);
-
-    verify(this.gameRepository).deleteById(1L);
-  }
-
-  @Test
-  public void deleteById_ThrowGameNotFoundException_InvalidId() {
-    when(this.gameRepository.findById(1L)).thenReturn(Optional.empty());
-    assertThrows(GameNotFoundException.class, () -> this.gameService.deleteById(1L));
-    verify(this.gameRepository).findById(1L);
-  }
-
-  @Test
-  public void create_CreateNewGame_GameWithNonExistingGameCode() {
-    Game expectedGame = new Game(GAMECODE);
-    expectedGame.setId(1L);
-    Game[] resultGame = new Game[1];
-
-    when(this.gameRepository.save(any()))
-        .then(invocationOnMock -> {
-          Game game = (Game) invocationOnMock.getArguments()[0];
-          game.setId(1L);
-          resultGame[0] = game;
-          return game;
-        });
-
-    when(this.gameRepository.getGameByGameCode(GAMECODE))
-        .thenReturn(Optional.empty())
-        .thenReturn(Optional.of(expectedGame));
-
-    this.gameService.create(new Game(GAMECODE));
-
-    assertNotNull(resultGame);
-    assertEquals(expectedGame.getId(), resultGame[0].getId());
-    assertEquals(expectedGame.getGameCode(), resultGame[0].getGameCode());
-    assertEquals(expectedGame.getAlchemists(), resultGame[0].getAlchemists());
-
-    verify(this.gameRepository).save(any());
-    verify(this.gameRepository, times(2)).getGameByGameCode(GAMECODE);
-  }
-
-  @Test
-  public void create_ThrowGameIsAlreadyExistException_GameWithExistingGameCode() {
-    Game test = new Game(GAMECODE);
-
-    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(test));
-
-    assertThrows(GameIsAlreadyExistException.class, () -> this.gameService.create(new Game(GAMECODE)));
-    verify(this.gameRepository).getGameByGameCode(GAMECODE);
-  }
-
-  @Test
-  public void update_UpdateGameCodeCorrectly_GameWithExistingGameCode() {
-    Game game = new Game(GAMECODE);
-    game.setId(1L);
-    Game[] resultGame = new Game[1];
-
-    when(this.gameRepository.findById(1L)).thenReturn(Optional.of(game));
-    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(game));
-    when(this.gameRepository.save(any()))
-        .then(invocationOnMock -> {
-          resultGame[0] = (Game) invocationOnMock.getArguments()[0];
-          return resultGame[0];
-        });
-
-    this.gameService.update(1L, game);
-
-    assertNotNull(resultGame[0]);
-    assertEquals(game.getGameCode(), resultGame[0].getGameCode());
-
-    verify(this.gameRepository).save(any());
-    verify(this.gameRepository).findById(1L);
-    verify(this.gameRepository).getGameByGameCode(GAMECODE);
-  }
-
-  @Test
-  public void update_ThrowGameNotFoundException_GameWithNonExistingGameCode() {
-    when(this.gameRepository.findById(1L)).thenReturn(Optional.empty());
-    assertThrows(GameNotFoundException.class, () -> this.gameService.update(1L, new Game(GAMECODE)));
-    verify(this.gameRepository).findById(1L);
+    this.merchants = new ArrayList<>(Arrays.asList(
+        new ObtainIngrMerchant(1, 1, 1, 1),
+        new TradeIngrMerchant(0, 0, 0, 1, 0, 0, 2, 0),
+        new UpdateIngrMerchant(3)));
   }
 
   @Test
@@ -314,6 +191,185 @@ class GameServiceImplTest {
     gameServiceMock.setStarterMerchants(new ArrayList<>(), new ArrayList<>());
 
     verify(gameServiceMock, times(1)).setStarterMerchants(new ArrayList<>(), new ArrayList<>());
+  }
+
+  @Test
+  public void setStarterUnavailableMerchants_ReturnAllMerchants_ThereAre3Merchants() {
+    when(this.merchantService.findStarterUnavailableMerchants()).thenReturn(this.merchants);
+
+    List<Merchant> result = this.gameService.setStarterUnavailableMerchants(new ArrayList<>());
+
+    assertNotNull(result);
+    assertEquals(3, result.size());
+
+    verify(this.merchantService).findStarterUnavailableMerchants();
+  }
+
+  @Test
+  public void setStarterAvailablefMerchants_Return6RandomMerchants_ThereAre7Merchants() {
+    this.merchants.addAll(new ArrayList<>(Arrays.asList(
+        new UpdateIngrMerchant(3),
+        new UpdateIngrMerchant(3),
+        new UpdateIngrMerchant(3),
+        new UpdateIngrMerchant(3))));
+
+    when(this.randomNumberMock.nextInt(anyInt())).thenReturn(0);
+
+    List<Merchant> result = this.gameService.setStarterAvailableMerchants(this.merchants, new ArrayList<>());
+
+    assertNotNull(result);
+    assertEquals(6, result.size());
+
+    verify(this.randomNumberMock, times(6)).nextInt(anyInt());
+  }
+
+  @Test
+  public void setStarterHands_SetStarterMerchantCardsAndReturnAlchemists_ThereAreTwoAlchemists() {
+    this.merchants.remove(1);
+    when(this.merchantService.pickUpStarterCards()).thenReturn(this.merchants);
+
+    List<Alchemist> result = this.gameService.setStarterHands(this.alchemists);
+
+    assertNotNull(result);
+    assertEquals(2, result.get(0).getMerchantsInHand().size());
+    assertEquals(2, result.get(1).getMerchantsInHand().size());
+
+    verify(this.merchantService).pickUpStarterCards();
+  }
+
+  @Test
+  public void getAll_ReturnAllGames_TwoGames() {
+    Game test1 = new Game(GAMECODE + "1");
+    Game test2 = new Game(GAMECODE + "2");
+    List<Game> games = new ArrayList<>(Arrays.asList(test1, test2));
+
+    when(this.gameRepository.findAll()).thenReturn(games);
+
+    List<Game> result = this.gameService.getAll();
+
+    assertNotNull(result);
+    assertEquals(2, result.size());
+    assertEquals(games, this.gameService.getAll());
+
+    verify(this.gameRepository, times(2)).findAll();
+  }
+
+  @Test
+  public void getAll_ReturnEmptyList_NoGame() {
+    when(this.gameRepository.findAll()).thenReturn(new ArrayList<>());
+    assertEquals(new ArrayList<>(), this.gameService.getAll());
+    verify(this.gameRepository).findAll();
+  }
+
+  @Test
+  public void getGameByGameCode_ReturnGame_GameWithExistingGameCode() {
+    Game test = new Game(GAMECODE);
+
+    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(test));
+
+    Game result = this.gameService.getGameByGameCode(GAMECODE);
+
+    assertNotNull(result);
+    assertEquals(GAMECODE, result.getGameCode());
+    assertEquals(test, this.gameService.getGameByGameCode(GAMECODE));
+
+    verify(this.gameRepository, times(2)).getGameByGameCode(GAMECODE);
+  }
+
+  @Test
+  public void getGameByGameCode_ThrowGameNotFoundException_GameWithNonExistingGameCode() {
+    when(this.gameRepository.getGameByGameCode("notExist")).thenReturn(Optional.empty());
+    assertThrows(GameNotFoundException.class, () -> this.gameService.getGameByGameCode("notExist"));
+  }
+
+  @Test
+  public void create_CreateNewGame_GameWithNonExistingGameCode() {
+    Game expectedGame = new Game(GAMECODE);
+    expectedGame.setId(1L);
+    Game[] resultGame = new Game[1];
+
+    when(this.gameRepository.save(any()))
+        .then(invocationOnMock -> {
+          Game game = (Game) invocationOnMock.getArguments()[0];
+          game.setId(1L);
+          resultGame[0] = game;
+          return game;
+        });
+
+    when(this.gameRepository.getGameByGameCode(GAMECODE))
+        .thenReturn(Optional.empty())
+        .thenReturn(Optional.of(expectedGame));
+
+    this.gameService.create(new Game(GAMECODE));
+
+    assertNotNull(resultGame);
+    assertEquals(expectedGame.getId(), resultGame[0].getId());
+    assertEquals(expectedGame.getGameCode(), resultGame[0].getGameCode());
+    assertEquals(expectedGame.getAlchemists(), resultGame[0].getAlchemists());
+
+    verify(this.gameRepository).save(any());
+    verify(this.gameRepository, times(2)).getGameByGameCode(GAMECODE);
+  }
+
+  @Test
+  public void create_ThrowGameIsAlreadyExistException_GameWithExistingGameCode() {
+    Game test = new Game(GAMECODE);
+
+    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(test));
+
+    assertThrows(GameIsAlreadyExistException.class, () -> this.gameService.create(new Game(GAMECODE)));
+    verify(this.gameRepository).getGameByGameCode(GAMECODE);
+  }
+
+  @Test
+  public void deleteById_SoftDeleteGame_ValidId() {
+    Game test = new Game(GAMECODE);
+    test.setId(1L);
+
+    when(this.gameRepository.findById(1L)).thenReturn(Optional.of(test));
+    doNothing().when(this.gameRepository).deleteById(1L);
+
+    this.gameService.deleteById(1L);
+
+    verify(this.gameRepository).deleteById(1L);
+  }
+
+  @Test
+  public void deleteById_ThrowGameNotFoundException_InvalidId() {
+    when(this.gameRepository.findById(1L)).thenReturn(Optional.empty());
+    assertThrows(GameNotFoundException.class, () -> this.gameService.deleteById(1L));
+    verify(this.gameRepository).findById(1L);
+  }
+
+  @Test
+  public void update_UpdateGameCodeCorrectly_GameWithExistingGameCode() {
+    Game game = new Game(GAMECODE);
+    game.setId(1L);
+    Game[] resultGame = new Game[1];
+
+    when(this.gameRepository.findById(1L)).thenReturn(Optional.of(game));
+    when(this.gameRepository.getGameByGameCode(GAMECODE)).thenReturn(Optional.of(game));
+    when(this.gameRepository.save(any()))
+        .then(invocationOnMock -> {
+          resultGame[0] = (Game) invocationOnMock.getArguments()[0];
+          return resultGame[0];
+        });
+
+    this.gameService.update(1L, game);
+
+    assertNotNull(resultGame[0]);
+    assertEquals(game.getGameCode(), resultGame[0].getGameCode());
+
+    verify(this.gameRepository).save(any());
+    verify(this.gameRepository).findById(1L);
+    verify(this.gameRepository).getGameByGameCode(GAMECODE);
+  }
+
+  @Test
+  public void update_ThrowGameNotFoundException_GameWithNonExistingGameCode() {
+    when(this.gameRepository.findById(1L)).thenReturn(Optional.empty());
+    assertThrows(GameNotFoundException.class, () -> this.gameService.update(1L, new Game(GAMECODE)));
+    verify(this.gameRepository).findById(1L);
   }
 
 }
